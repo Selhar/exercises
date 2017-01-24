b368c76486b2a7ccc5251be1a85606c9e4998e19
Selhar <gregoryjgoncalves@gmail.com>
2017-01-24 12:57:35 -0300

    fcc leaderboard

--- freecodecamp/leaderboard/src/App.js
+++ freecodecamp/leaderboard/src/App.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class Header extends Component {
-  render(){
-    return (
-      <header>Freecodecamp leaderboard </header>
-    )
-  }
-}
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        </Header>
+        <Leaderboard/>
       </div>
     );
   }
 }
+
+class Leaderboard extends Component {
+  render(){
+    return (
+      <div className="board">
+        <header>Freecodecamp leaderboard </header>
+        <table>
+          <tr>
+            <th>#</th>
+            <th>Camper</th>
+            <th>Points</th>
+          </tr>
+        </table>
+      </div>
+    )
+  }
+}
+
+
 
 export default App;
