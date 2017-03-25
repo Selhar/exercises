cd7d40181b39af16e96a69f4f4d218ae0fabfac6
selhar <selhar@protonmail.com>
2017-03-25 07:33:47 -0300

    cleaning up

--- rust/rust_essentials/chapter_2/src/main.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let mut a = 5;
-    let mut b = 6;
-    let n = 7;
-    let a = b = n;
-    println!("{:?}{:?}{:?}", a, b, n);
-}
--- rust/rust_essentials/chapter_3/src/doc/main/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"fn":[["cube","teste the cube `val * val * val`."]]});
