e92b8ff5531bcc5ab7a5bad78341a9ddad078f8b
Selhar <selhar@protonmail.com>
2017-05-27 18:09:19 -0300

    ex11

---
+++ learn_python_hard_way/ex11.py
@@ -0,0 +1,8 @@
+print "How old are you?",
+age = raw_input()
+print "how tall are you?",
+height = raw_input()
+print "How much do you weight?",
+weight = raw_input()
+
+print "You are %r old, %r tall and %r heavy" % (age,height, weight)
