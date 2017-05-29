2d621b723228c2858fc285ae7e17ebc13fcd80dd
Selhar <selhar@protonmail.com>
2017-05-29 19:10:03 -0300

    removed node modules, switched to yarn, new dependencies

--- graphQL/projeto_teste/node_modules/graphql-server-koa/README.md
+++
@@ -1,3 +0,0 @@
-# graphql-server-koa
-
-This is the Koa integration for the Apollo community GraphQL Server. [Read the docs.](http://dev.apollodata.com/tools/apollo-server/index.html)
--- graphQL/projeto_teste/src/server.js
+++ graphQL/projeto_teste/src/server.js
@@ -2,8 +2,7 @@ import koa from 'koa';
 import koa_router from 'koa-router';
 import koa_body from 'koa-bodyparser';
 import {graphqlKoa, graphiqlKoa} from 'graphql-server-koa';
-
-//import schema from './data/schema';
+import schema from './data/schema';
 import './db';
 
 const server = new koa();
@@ -12,9 +11,8 @@ const PORT = 3000;
 
 server.use(koa_body());
 
-//router.post('/graphql', graphqlKoa({schema: schema}));
-//router.get('/graphql', graphqlKoa({scheme: schema}));
-
+router.post('/graphql', graphqlKoa({schema: schema}));
+router.get('/graphql', graphqlKoa({scheme: schema}));
 router.get('/graphiql', graphiqlKoa({endpointURL: '/graphql'}));
 
 server.use(router.routes());
