6ae94fdaafb72681a3c2745ef766cd30adc4d56c
Selhar <selhar@protonmail.com>
2017-05-30 04:42:14 -0300

    fetching api data

--- react_native/AwesomeProject/index.android.js
+++ react_native/AwesomeProject/index.android.js
@@ -13,21 +13,28 @@ import {
   View
 } from 'react-native';
 
-const MOCKED_MOVIES_DATA = [
-  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
-];
+const REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
 
 export default class AwesomeProject extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: null,
+    };
+  }
+  
   render() {
     var movie = MOCKED_MOVIES_DATA[0];
     return (
       <View style={styles.container}>
-        <Text>{movie.title}</Text>
-        <Text>{movie.year}</Text>
         <Image
           style={styles.thumbnail} 
           source={{uri: movie.posters.thumbnail}} />
-      </View>
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
+    </View>
     );
   }
 }
@@ -35,6 +42,7 @@ export default class AwesomeProject extends Component {
 const styles = StyleSheet.create({
    container: {
       flex: 1,
+      flexDirection: 'row',
       justifyContent: 'center',
       alignItems: 'center',
       backgroundColor: '#F5FCFF'
@@ -42,6 +50,17 @@ const styles = StyleSheet.create({
     thumbnail: {
       width: 53,
       height: 81
+    },
+    rightContainer: {
+      flex: 1,
+    },
+    title: {
+      fontSize: 20,
+      marginBottom: 8,
+      textAlign: 'center',
+    },
+    year: {
+      textAlign: 'center'
     }
 });
 
