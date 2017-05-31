37b0adda6f131e2246205e37b49cb611c8dd7430
Selhar <selhar@protonmail.com>
2017-05-31 10:53:59 -0300

    data fetch issue - CORS && node_modules bug

--- entria/FrontEnd/src/NewUser.js
+++ entria/FrontEnd/src/NewUser.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import request from 'request';
 
 export default class Teste extends Component {
   constructor(props) {
@@ -14,9 +15,20 @@ export default class Teste extends Component {
     title: 'Add a new user',
   };
 
-  sendUserData() {
-    fetch('http://localhost:5000/graphql/',{ method: "POST", body: "boga"})
+/*
+endereço: http://localhost:5000/graphql/
+Query pra adição de e-mail:
 
+query: ```
+      mutation {
+        RegisterEmail(input: {name: "abc" email: "abc@dfg.com" password: "asd"}){
+          token
+        }
+      }
+       ```
+*/
+  sendUserData() {
+    request.post('http://localhost:5000/graphql/', {form:{key:'teste'}})
   }
 
   render() {
