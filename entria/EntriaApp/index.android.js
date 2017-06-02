73afc6a0b0e0397cafb85310da9cbb2b54dd818a
Selhar <selhar@protonmail.com>
2017-06-02 15:52:44 -0300

    partially successful render

--- entria/EntriaApp/index.android.js
+++ entria/EntriaApp/index.android.js
@@ -5,25 +5,46 @@
  */
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-import Relay, { 
-  Route,
-  RootContainer,
-  DefaultNetworkLayer
-} from 'react-relay/classic'
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import Relay, { Route, RootContainer, DefaultNetworkLayer } from 'react-relay/classic';
 
 Relay.injectNetworkLayer(new DefaultNetworkLayer('http://localhost:5000/graphql'))
 
-export default class EntriaApp extends Component {
-  render() {
+class ViewerQueryRoute extends Route {
+  static queries = {
+    viewer: () => Relay.QL` query { viewer } `
+  }
+  static routeName = 'ViewerQueryRoute'
+}
+
+class UserInfo extends Component {
+  render () {
     return (
-    );
+      <Text>User Length: {this.props.viewer.id}</Text>
+    )
   }
 }
-AppRegistry.registerComponent('EntriaApp', () => EntriaApp);
+
+UserInfo = Relay.createContainer(UserInfo, {
+  fragments: {
+    viewer: () => Relay.QL`
+    fragment on Viewer {
+      id
+    }`
+  }
+})
+
+//console.log('\n\n\n\n\n'+UserInfo+"\nXX\n"+ViewerQueryRoute);
+class RenderAttempt extends Component {
+  render() {
+    return (
+      <RootContainer
+        Component={UserInfo}
+        route={new ViewerQueryRoute()}
+
+      />
+    )
+  }
+}
+
+AppRegistry.registerComponent('EntriaApp', () => RenderAttempt);
