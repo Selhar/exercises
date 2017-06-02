a120978ff52968a078f97502bb207221e19de87a
Selhar <selhar@protonmail.com>
2017-06-02 01:56:32 -0300

    backend

---
+++ react_native/AwesomeProject/src/app.js
@@ -0,0 +1,113 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  Image,
+  ListView,
+  View
+} from 'react-native';
+
+const REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
+export default class AwesomeProject extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+		  rowHasChanged: (row1, row2) => row1 !== row2,
+	  }),
+	  loaded: false,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((response_data) => {
+        this.setState({
+          	dataSource: 
+		  		this.state.dataSource.cloneWithRows(response_data.movies),
+			loaded: true,
+        });
+      }).done();
+  }
+
+  	render() {
+    	if(!this.state.loaded){
+			return this.renderLoadingView();
+		}
+        // const {container, thumbnail, etc..} = styles
+		return(
+			<ListView 
+				dataSource={this.state.dataSource}
+				renderRow={this.renderMovie}
+				styles={styles.listView}
+			/>
+		);
+	}
+
+	renderLoadingView() {
+		return (
+			<View style={styles.container}>
+				<Text>
+					Loading movies...
+				</Text>
+			</View>
+		);
+	}
+
+	renderMovie(movie) {
+		return (
+			<View style={styles.container}>
+				<Image
+					source={{uri: movie.posters.thumbnail}}
+					style={styles.thumbnail}
+				/>
+				<View style={styles.rightContainer}>
+					<Text style={styles.title}>{movie.title}</Text>
+					<Text style={styles.year}>{movie.year}</Text>
+				</View>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF'
+    },
+    thumbnail: {
+      width: 53,
+      height: 81
+    },
+    rightContainer: {
+      flex: 1,
+    },
+    title: {
+      fontSize: 20,
+      marginBottom: 8,
+      textAlign: 'center',
+    },
+    year: {
+      textAlign: 'center'
+    },
+	listView: {
+		paddingTop: 20,
+		backgroundColor: '#F5FCFF'
+	}
+});
+
