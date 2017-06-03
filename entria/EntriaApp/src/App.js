185eb07363a5aa75daa7e55b18fd55f7f8850643
Selhar <selhar@protonmail.com>
2017-06-03 00:47:44 -0300

    styles and removed not refactored files from rendering

--- entria/EntriaApp/src/App.js
+++ entria/EntriaApp/src/App.js
@@ -3,7 +3,6 @@ import Relay, { RootContainer, DefaultNetworkLayer } from 'react-relay/classic';
 import ViewerQueryRoute from './views/ViewerQueryRoute';
 //Adding all screens here for testing, at the end it should only be Menu.js
 import UserList from './ListUsers';
-import AddUser from './NewUser'
 
 // *****HAS TO BE INTERNAL IP. LOCALHOST DOESN'T WORK ON ANDROID.*******
 Relay.injectNetworkLayer(new DefaultNetworkLayer('http://192.168.25.216:5000/graphql'));
--- entria/EntriaApp/src/ListUsers.js
+++ entria/EntriaApp/src/ListUsers.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
-import { FlatList, Text } from 'react-native';
+import { FlatList, Text, StyleSheet } from 'react-native';
 import Relay from 'react-relay/classic';
 
 class UserList extends Component {
   render () {
    //Print the object array for debugging
    //console.log('\n\n\n\n\n\n\n'+JSON.stringify(this.props.viewer.users.edges, null, 4));
-
    let data_root = this.props.viewer.users.edges;
     return (
-      <FlatList
+      <FlatList style={styles.list}
         data={data_root} 
         keyExtractor={(item) => item.node.id}
-        renderItem={({item}) => <Text>{item.node.name}</Text>} 
+        renderItem={({item}) => <Text style={styles.item}>{item.node.name}</Text>} 
       />
     )
   }
 }
+
+const styles = StyleSheet.create({ 
+  list: { 
+    flexDirection: 'row', 
+    flexWrap: 'wrap', 
+  }, 
+  item: { 
+    fontSize: 25, 
+  }, 
+});
 
 export default UserList = Relay.createContainer(UserList, {
   fragments: {
