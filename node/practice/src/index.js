f29c12ee0d5c90e03c4d5d00add76349f4e2d8e9
Selhar <selhar@protonmail.com>
2017-06-10 08:47:09 -0300

    header, footer

---
+++ node/practice/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+
+const Index = () => {
+    return (
+        <h2>o2i</h2>
+    );
+};
+
+reactDOM.render(
+    <Index />,
+    document.getElementById('root')
+)
