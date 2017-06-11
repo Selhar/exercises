9dfcd864a4bb50afbcad911077ae9532123bf0f9
Selhar <selhar@protonmail.com>
2017-06-11 12:55:40 -0300

    sass integration

--- node/practice/server.js
+++ node/practice/server.js
@@ -1,18 +1,25 @@
 import config from './config';
 import express from 'express';
 import api_router from './api';
+import sass from 'node-sass-middleware';
+import path from 'path';
+
 
 const server = express();
 
 server.set('view engine', 'ejs');
 
+server.use('/api', api_router);
+server.use(express.static('public'));
+server.use(sass({
+    src: path.join(__dirname + '/views/sass'),
+    dest: path.join(__dirname + '/public'),
+}));
+
 server.get('/', (request, response) => {
     response.render('index');
 });
 
-server.use('/api', api_router);
-server.use(express.static('public'));
-
 server.listen(config.port, () => {
     console.info('Listening on port: ', config.port);
-})
+});
