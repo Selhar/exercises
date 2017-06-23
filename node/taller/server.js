9b95d8deeb1d8b0a2f87cf93c6126185343d6926
Selhar <selhar@protonmail.com>
2017-06-23 14:32:31 -0300

    boilerplate setup

--- node/taller/server.js
+++ node/taller/server.js
@@ -15,8 +15,6 @@ const mongoose = require('mongoose');
 const server = express();
 const root = process.cwd();
 const api_root = '/';
-const ThreadModel = require('./models/Thread');
-const ObjectId = require('mongodb').ObjectID;
 
 mongoose.connect('mongodb://localhost:27017/taller');
 
@@ -25,30 +23,9 @@ server.use(body_parser());
 server.use(body_parser.json());
 server.use(body_parser.urlencoded( {extended: true} ));
 
-/*
-The freecodecamp test suite require the server response to be on JSON
-I can't return JSON to render a page, thus 2 server responses were created
-They're pretty much similar, but tests should never be done this way. (the official solution is also similar)
-
-This curriculum is still in beta so this might be an oversight
-either way, to test the project just uncomment below.
-
-Also, routes are slightly different because you can't DELETE through http forms
-
-
-        **  Uncomment these for testing  **
-
-server.get(api_root, thread_controller.fetch); 
-server.get(api_root+':thread_id', thread_controller.fetchById);
-server.post(api_root, thread_controller.create); 
-server.delete(api_root, thread_controller.remove);
-server.put(api_root, thread_controller.report);
-
-server.post(api_root+':thread', replies_controller.create); 
-server.put(api_root+':thread_id', replies_controller.report);
-server.delete(api_root+':thread_id', replies_controller.remove);
-
-*/
+server.get(api_root, (request, response) => {
+	response.render(root + '/views/login.ejs');
+});
 
 server.listen(process.env.PORT || 3000, () => {
     console.log("Listening");
