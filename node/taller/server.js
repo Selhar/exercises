3cba08371b22d2760c87d8c8fb7e9c8e01df78a5
Selhar <selhar@protonmail.com>
2017-06-23 10:19:55 -0300

    boilerplate

--- node/taller/server.js
+++ node/taller/server.js
@@ -10,7 +10,6 @@
 */
 
 const express = require('express');
-const helmet = require('helmet');
 const body_parser = require('body-parser');
 const mongoose = require('mongoose');
 const server = express();
@@ -19,21 +18,12 @@ const api_root = '/';
 const ThreadModel = require('./models/Thread');
 const ObjectId = require('mongodb').ObjectID;
 
-mongoose.connect("mongodb://localhost:27017/anonymous-imageboard");
-
-server.use(helmet({
-    referrerPolicy: true,
-    referrerPolicy: {
-        policy: 'same-origin'
-    }
-}));
+mongoose.connect('mongodb://localhost:27017/taller');
 
 server.use('/public', express.static(root + '/public'));
 server.use(body_parser());
 server.use(body_parser.json());
 server.use(body_parser.urlencoded( {extended: true} ));
-
-
 
 /*
 The freecodecamp test suite require the server response to be on JSON
@@ -59,140 +49,6 @@ server.put(api_root+':thread_id', replies_controller.report);
 server.delete(api_root+':thread_id', replies_controller.remove);
 
 */
-
-
-//Get complete thread list
-server.get(api_root, (request, response) => {
-    ThreadModel.find({})
-                .select('_id creation_date last_post text title replies')
-                .sort({creation_date: '-1'})
-                .limit(10).exec((error, threads) => {
-        if(error){
-            throw error;
-        }else if(threads){
-            response.render(root + '/views/board.ejs', {data: threads});
-        }
-    });
-});
-
-//Show a single thread
-server.get(api_root+':thread_id', (request, response) => {
-
-    ThreadModel.findById(request.params.thread_id)
-                .select('_id creation_date last_post text title replies')
-                .exec((error, thread) =>{
-        if(error){
-            response.send("Bad, bad user. No donuts for you.")
-        }else if(thread){
-            response.render(root + '/views/thread.ejs', {
-                data: thread,
-                home_url: 'http://'+request.headers.host
-            });
-        }else{
-            response.send("thread not found");
-        }
-    });
-});
-
-//Make a new thread
-server.post(api_root, (request, response) => {
-    let url_user_came_from = request.protocol + '://' + request.get('Host');
-    
-    let new_thread = new ThreadModel({
-        title: request.body.title,
-        text: request.body.text,
-        password: request.body.password
-    });
-
-    new_thread.save((error) => {
-        if(error){
-            throw error;
-        }
-        response.redirect(url_user_came_from+'/'+new_thread._id);
-    });
-});
-
-//Delete a thread
-server.get(api_root+'delete', (request, response) => {
-    let url_user_came_from = request.protocol + '://' + request.get('Host');
-    ThreadModel.findOneAndRemove({
-        _id: request.query.thread_id,
-        password: request.query.password
-    }, (error, thread) => {
-        if(error)
-            throw error
-        else
-            response.redirect(url_user_came_from);
-    });
-
-});
-
-//report a thread
-server.get(api_root+'report/:thread', (request, response) => {
-    ThreadModel.findByIdAndUpdate(request.params.thread, 
-                {isReported: true}, (error, thread) => {
-        if(error)
-            throw error;
-        else if(thread)
-            response.send('Report successful.');
-        else
-            response.send('Thread ID not found');
-    });
-});
-
-//Make a new comment
-server.post(api_root+':thread', (request, response) => {
-    let url_user_came_from = request.protocol + '://' + request.get('Host');
-    let reply = {
-            text: request.body.text, 
-            password: request.body.password,
-            _id: new ObjectId()
-    }
-    ThreadModel.findByIdAndUpdate(request.body.thread_id, {
-        $push: {replies: reply}
-    }, (error, data) => {
-        if(error)
-            throw error;
-        else
-            response.redirect(url_user_came_from+'/'+request.body.thread_id);
-    })
-});
-
-//Delete a comment
-server.get(api_root+'delete/:thread', (request, response) => {
-    ThreadModel.update({'_id': request.params.thread}, 
-        {'$pull': 
-            {'replies': 
-                {'_id': request.query.reply_id}
-            }
-        }, (error) => {
-            if(error){
-                callback(error);
-            }else{
-                response.redirect('http://'+request.headers.host);
-            }
-        }
-    );
-});
-
-//report a comment
-server.get(api_root+'reportcomment/:reply', (request, response) => {
-    ThreadModel.update({'replies._id': request.params.reply}, 
-        {'$set': {'replies.$.isReported': true}}, (error, thread) => {
-            if(error){
-                throw error;
-            }else if(thread){
-                response.send("Reported successfully");
-            }else{
-                response.send("Report not successful");
-            }
-        }
-    );
-});
-
-server.get('*', (request, response) => {
-    response.send('<p>Bad, bad user. No donuts for you.</p>');
-});
 
 server.listen(process.env.PORT || 3000, () => {
     console.log("Listening");
