d433fad4d28bb6bdb813df6a64875aded4199342
Selhar <selhar@protonmail.com>
2017-08-26 19:22:53 -0300

    tests

---
+++ rails_tutorial/sample_app/test/models/user_test.rb
@@ -0,0 +1,48 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name: "Example user", email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "   "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "   "
+    assert_not @user.valid?
+  end
+
+  test "name shouldn't be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email shouldn't be too long" do
+    @user.email = "a" * 290 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "valid emails should be accepted" do
+    valid_emails = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                      first.last@foo.jp alice+bob@baz.cn]
+    valid_emails.each do |valid_email|
+      @user.email = valid_email
+      assert @user.valid?, "#{valid_email.inspect} should be valid"
+    end
+  
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+end
