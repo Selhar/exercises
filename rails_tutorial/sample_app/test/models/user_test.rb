77bb219b6b83469d65cc005bcc30645f927a37bf
Selhar <selhar@protonmail.com>
2017-08-26 21:32:59 -0300

    password hashing

--- rails_tutorial/sample_app/test/models/user_test.rb
+++ rails_tutorial/sample_app/test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example user", email: "user@example.com")
+    @user = User.new(name: "Example user", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -45,5 +45,12 @@ class UserTest < ActiveSupport::TestCase
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
   end
 end
